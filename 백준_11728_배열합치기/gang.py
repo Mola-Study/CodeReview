6366058b20da9149780d8d60259b14a88d855740
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-02-12 16:56:09 +0900

    Update gang.py

--- 백준_11728_배열합치기/gang.py
+++ 백준_11728_배열합치기/gang.py
@@ -3,7 +3,7 @@ N, M = map(int,input().split())
 liA = list(map(int,input().split()))
 liB = list(map(int,input().split()))
 
-res = liA + liB
-res.sort()
+liA = liA + liB
+liA.sort()
 
-print(*res)
+print(*liA)
