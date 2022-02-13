45f60f20cb9b7bd7c89e78bc6229edcc2d7dfd2f
beeee2 <beeee2@naver.com>
2022-02-13 16:41:59 +0900

    Clear 11728 by sueun

--- 백준_11728_배열합치기/sueun.py
+++ 백준_11728_배열합치기/sueun.py
@@ -1,1 +1,10 @@
-# 이수은_백준11728_배열합치기_풀이 
+# 이수은_백준11728_배열합치기_풀이
+
+condition = map(int, input().split())
+array_first = list(map(int, input().split()))
+array_second = list(map(int, input().split()))
+
+new_array = (array_first + array_second)
+new_array.sort()
+for i in new_array:
+    print(i, end=" ")
