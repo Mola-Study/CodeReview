5102cee1c61c9f3a3c8e945d522fb91304702e28
MeaninGood <jsmeaningood@gmail.com>
2022-02-20 22:34:01 +0900

    22.02.20

--- 백준_16561_3의 배수/sun.py
+++ 백준_16561_3의 배수/sun.py
@@ -1,9 +1,9 @@
 n = int(input())
 
 cnt = 0
-for i in range(3, n+1, 3):
+for i in range(3, n+1, 3): # 3부터 3 간격으로 이중 for문 돌리기
     for j in range(3, n+1, 3):
-        if i + j < n:
+        if i + j < n: # 숫자 2개가 정해지면 하나는 자동으로 정해짐
             cnt += 1
             
 print(cnt)
