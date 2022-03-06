c7bd7678990a6adb488dfff842bd14e3fbd09e74
beeee2 <beeee2@naver.com>
2022-03-06 23:17:06 +0900

    Clear baekjoon problem No.17945

--- 백준_17945_통학의 신/sueun.py
+++ 백준_17945_통학의 신/sueun.py
@@ -1,1 +1,9 @@
-# 이수은_백준2309_일곱난쟁이_풀이 
+import math
+A, B = map(int, input().split())
+
+res2 = int((-2*A + (math.sqrt(math.pow(2*A,2) - 4 * B)))/2)
+res1 = int((-2*A - (math.sqrt(math.pow(2*A,2) - 4 * B)))/2)
+if res1 == res2:
+    print(res1)
+else:
+    print(res1, res2)
