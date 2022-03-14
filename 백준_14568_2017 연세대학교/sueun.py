3f36c1269cc8b2bbc5ea9e5e56855fde4cd45ee4
beeee2 <beeee2@naver.com>
2022-03-14 23:30:47 +0900

    Clear no.12458

--- 백준_14568_2017 연세대학교/sueun.py
+++ 백준_14568_2017 연세대학교/sueun.py
@@ -1,1 +1,12 @@
-# 이수은_백준2309_일곱난쟁이_풀이 
+N = int(input())
+cnt = 0
+for i in range(1, N-1):
+    x = i
+    for j in range(i+2, N+1):
+        y = N - i - j
+        if y > 0 and y % 2 == 0:
+            cnt += 1
+if cnt:
+    print(cnt)
+else:
+    print(0)
