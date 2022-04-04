6abed146382e0a818ee78b5bf237303dc35986f5
beeee2 <beeee2@naver.com>
2022-04-04 23:32:30 +0900

    [추가] 문제 폴더 업로드

--- 백준_13423_Three Dots/sueun.py
+++ 백준_13423_Three Dots/sueun.py
@@ -1,1 +1,46 @@
-#수은
+
+
+import sys
+T = int(sys.stdin.readline())
+
+for test_case in range(T):
+    N = int(sys.stdin.readline())
+    lst = sorted([int(i) for i in sys.stdin.readline().split()])
+
+    cnt = 0
+    for i in range(len(lst)-2):
+        for j in range(i+1, len(lst)-1):
+            for k in range(j+1, len(lst)):
+                if (abs(lst[j] - lst[i]) == abs(lst[k] - lst[j])):
+                        cnt += 1
+                        break
+                elif (abs(lst[j] - lst[i]) > abs(lst[k] - lst[j])):
+                    continue
+                else:
+                    break
+
+    # for i in range(len(lst)-2):
+    #     for j in range(i+1, len(lst)-1):
+    #         if (abs(lst[j] - lst[i]) == abs(lst[j+1] - lst[j])):
+    #             cnt += 1
+    #             break
+    #         elif (abs(lst[j] - lst[i]) > abs(lst[j+1] - lst[j])):
+    #             continue
+    #         else:
+    #             break
+    print(cnt)
+
+# # lst = [1,3,2,5,4]
+#     lst.sort()
+#     cnt = 0
+#     for i in range(len(lst)-2):
+#         for j in range(i+1, len(lst)-1):
+#             for k in range(j+1, len(lst)):
+#                 if (abs(lst[j] - lst[i]) == abs(lst[k] - lst[j])):
+#                     cnt += 1
+#                     break
+#                 elif (abs(lst[j] - lst[i]) < abs(lst[k] - lst[j])):
+#                     break
+#     print(cnt)
+
+# 이따가 재귀로 짜보자
--- 백준_1697_숨바꼭질/sueun.py
+++ 백준_1697_숨바꼭질/sueun.py
@@ -1,1 +1,23 @@
 #수은
+
+import sys
+N, K = [int(i) for i in sys.stdin.readline().split()]
+cnt = 0
+s = N
+e = 100000
+while True:
+    mid = (s + e)//2
+    if K < mid:
+        cnt += 1
+        e = mid
+    elif K > mid:
+        cnt += 1
+        s = mid
+    else:
+        cnt += 1
+        break
+print(cnt)
+'''
+위 식의 문제점 : 
+-1로 가는 경우랑 +1로 가는경우 2배만큼 가는 경우를 고려하지 않았다.
+'''
