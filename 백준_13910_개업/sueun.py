6293247604ad76cee17a38fd34c0c4ec45610bf7
beeee2 <beeee2@naver.com>
2022-04-24 08:59:20 +0900

    [BOJ_15666] 수은/시간초과

--- 백준_13910_개업/sueun.py
+++ 백준_13910_개업/sueun.py
@@ -1,24 +1,40 @@
-# 이수은_백준2309_일곱난쟁이_풀이
+import sys
+from collections import deque
+# 시간초과, 6%
 
-'''
-접근방법 및 해결을 못한 이유:
-- 주어진 입력값을 오름차순으로 정렬 후,
-- 순서대로 7개를 차례차례 더하여 해당 값이 100이 넘는 지 확인하려고 하였다.
-- 100이 넘지 않을 경우 index 6번째에 해당하는 값을 빼고 7번째에 해당하는 값을 넣으려고 하였다.
-- 하지만 100을 만족하는 경우는 a, b, d, e, f, j, h 와 같이 앞서 주어진 요소가 빠지고
-- 큰 값이 들어오는 경우도 있었기에 위 접근방법으로는 문제를 풀지 못했다.
-'''
 
-# 2309번, 일곱 난쟁이
-lis = []
-sum = 0
+def permutation(lst, k):                        # 원본 리스트와 출력개수 k를 전달받는다.
+    empty = deque()
+    def generate(chosen):
+        if len(chosen) > k:                     # 가지치기, 출력개수보다 chosen의 개수가 커지면 return한다.
+            return
 
-for _ in range(9):
-    lis += [int(input())]
+        if len(chosen) == k:                    # 선택한 숫자의 개수가 출력 개수와 일치할 때,
+            temp = deque()                           # 임시 리스트를 선언한다.(초기화한다.)
+            for i in range(len(chosen) - 1):    # chosen의 원소들이 오름차순으로 정렬되어 있지않다면 return한다.
+                if chosen[i] > chosen[i+1]:
+                    return
+            for i in range(len(chosen)):        # chosen의 원소들이 오름차순으로 정렬되어 있다면
+                temp.append(chosen[i])          # 해당 요소들을 temp에 담은 후
+            test_lst.append(temp)               # test_list(2차원 배열)에 더해준다.
+            return                              # return을 통해 빠져나간다.
 
-lis.sort()
-for i in range(7):
-    sum += lis[i]
-    if sum > 100:
-        sum -= lis[6]
-        sum += lis[7]
+        for i in range(len(lst)):
+            chosen.append(lst[i])
+            generate(chosen)
+            chosen.pop()
+
+    generate(empty)
+
+N, M = map(int, sys.stdin.readline().split())                # 숫자 개수 N과 출력 개수 M을 입력받는다.
+arr = sorted(list(map(int, sys.stdin.readline().split())))   # 숫자들을 오름차순으로 표현하기 위해 입력받은 후 정렬한다.
+test_lst = deque()
+permutation(arr, M)
+test_lst = set(list(map(tuple, test_lst)))                   # 중복되는 수열을 여러번 출력하지 않기 위해 set을 통해 제거한다.
+test_lst = sorted(list(test_lst))                           # set을 사용하여서 순서가 엉망이므로 다시 정렬한다.
+# print(test_lst)
+for item in test_lst:                                       # 2차원 배열 내 수열들을 순서대로 출력한다.
+    res= list(item)
+    for i in res:
+        print(i, end=" ")
+    print()
